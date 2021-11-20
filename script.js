3900b52ad535c6c58b179f6b5b742a6721bcd2f2
daotunglam <lam90namdinh@gmail.com>
2021-11-20 21:36:53 +0100

    finish allmost design

---
+++ script.js
@@ -0,0 +1,10 @@
+// function show(id){
+//     document.getElementById(id).style.display = 'flex';
+//     document.getElementById(id).innerHTML += `
+//         <button class="btn btn-close position-absolute btn-sm" onclick="hide('${id}')"></button>
+//     `;
+// }
+// function hide(id){
+//     document.getElementById(id).style.display = 'none';
+//     document.getElementById(id).querySelector('button').style.display = `none`;
+// }
